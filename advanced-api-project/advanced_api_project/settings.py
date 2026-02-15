16260e57fb397f6ab8569891997224a6688eedf2
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2026-02-15 10:16:13 -0500

    templates

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER':'<db_username',
+        'PASSWORD':'<password>',
+        'HOST':'<db_hostname_or_ip',
+        'PORT':'<db_port',
     }
 }
 
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect
 from . forms import UserRegisterForm ,UserUpdateForm
 from django.contrib import messages
+from . models import User
+from django.contrib.auth import authenticate , login
+
 
 def register(request):
     if request.method=='POST':
         form=UserRegisterForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
 
         messages.success(request,"Account Created Successfully! You can now log in.")
@@ -14,3 +17,30 @@ def register(request):
         form=UserRegisterForm ()
 
     return render(request,'blog/register.html',{'form':form})
+
+
+def login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+#checks if a user with the provided username exists
+
+        if not User.objects.filter(username==username).exists():
+            #display an error message if the username does not exist
+            messages.error(request,"Invalid Username")
+            return redirect('/login/')
+        
+        user=authenticate(username=username,password=password)
+
+        if user is None:
+            #display an error message if authentication fails (invalid password)
+            messages.error(request,"Invalid password")
+            return redirect ('/login/')
+        
+        else:
+            login(request,user)
+            return redirect ('/home/')
+        
+    
+    return render(request, 'login.html')

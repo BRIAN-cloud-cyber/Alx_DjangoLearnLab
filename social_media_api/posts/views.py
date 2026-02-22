afc702091cd6ac8f4f8f8f55cbedc69ab2c80cde
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2026-02-22 01:47:01 -0500

    post

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -73,11 +73,9 @@ class LikePostView(APIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
 
-        Like, created = Like.objects.get_or_create(
-            user=request.user,
-            post=post
-        )
+        Like, created = Like.objects.get_or_create( user=request.user, post=post)
 
+           
         if not created:
             return Response(
                 {"detail": "You already liked this post."},

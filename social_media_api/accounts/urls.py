82c8503fa1f31ed9dd58c634283a02f6eef4061c
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2026-02-21 20:42:58 -0500

    urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import  LoginView, RegistrationView , ProfileView
 
 urlpatterns = [
-    path('register', RegistrationView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login'),
-    path('profile', ProfileView.as_view(), name='profile'),
+    path('register/', RegistrationView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
